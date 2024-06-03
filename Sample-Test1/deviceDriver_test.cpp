1686b9a1053f3f01c92d2984f30b981e1bfd79cc
DanielPlontia <plontia@gmail.com>
2024-06-03 13:18:16 +0900

    fix testcase

--- Sample-Test1/deviceDriver_test.cpp
+++ Sample-Test1/deviceDriver_test.cpp
@@ -1,3 +1,4 @@
+#include <stdexcept>
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 #include "../DeviceDriver/DeviceDriver.cpp"
@@ -13,7 +14,8 @@ TEST(DeviceDriverFixture, Read5times) {
 	DDMock mock;
 	DeviceDriver hw(&mock);
 	EXPECT_CALL(mock, read)
-		.Times(5);
+		.Times(5)
+		.WillRepeatedly(Return('A'));
 
 	int ret = hw.read(0);
 	cout << ret;
@@ -22,45 +24,38 @@ TEST(DeviceDriverFixture, Read5times) {
 TEST(DeviceDriverFixture, NotSameReadResult) {
 	DDMock mock;
 	DeviceDriver hw(&mock);
+
 	EXPECT_CALL(mock, read(0))
+		.Times(5)
 		.WillOnce(Return('A'))
 		.WillOnce(Return('A'))
 		.WillOnce(Return('A'))
-		.WillOnce(Return('F'))
-		.WillRepeatedly(Return('A'));
-	try {
-		int result = hw.read(0);
-		FAIL();
-	}
-	catch (exception e) {
-		// PASS
-	}
+		.WillOnce(Return('A'))
+		.WillOnce(Return('F'));
+
+	EXPECT_THROW(hw.read(0), ReadFailException);
 }
 
 TEST(DeviceDriverFixture, NormalWrite) {
 	DDMock mock;
 	DeviceDriver hw(&mock);
+
 	EXPECT_CALL(mock, read(0))
 		.Times(5)
 		.WillRepeatedly(Return(0xFF));
 	EXPECT_CALL(mock, write(0, 'B'))
 		.Times(1);
+
 	hw.write(0, 'B');
 }
 
 TEST(DeviceDriverFixture, AlreadyWrite) {
 	DDMock mock;
 	DeviceDriver hw(&mock);
+
 	EXPECT_CALL(mock, read(0))
 		.Times(5)
 		.WillRepeatedly(Return(0x49));
-	EXPECT_CALL(mock, write(0, 'B'))
-		.Times(0);
-	try {
-		hw.write(0, 'B');
-		FAIL();
-	}
-	catch (exception e) {
-		// PASS`
-	}
+
+	EXPECT_THROW(hw.write(0, 'B'), WriteFailException);
 }
